71c821f9e96ed0e6495a34c526b06cfc2b91c46f
julien <julien@marmelab.com>
2016-10-25 18:00:46 +0200

    Code refactoring

--- src/game/game.py
+++ src/game/game.py
@@ -13,7 +13,7 @@ def start():
     while not board.is_full():
 
         print_score(board)
-        print(board.render(board.get_legal_moves(current_color)))
+        print_ask_board(board, current_color)
 
         position_choice = ask_position(current_color)
 
@@ -21,13 +21,23 @@ def start():
             print("Invalid position, try again")
             position_choice = ask_position(current_color)
 
-        current_color = board.CELL_BLACK if current_color == board.CELL_WHITE else board.CELL_WHITE
+        current_color = get_reverse_color(current_color)
 
     #print("{0} WINS !".format(winner))
+
 
 def ask_position(color):
     return int(input("Player ({0}) which position ?".format(color)))
 
+
+def print_ask_board(board, color):
+    print(board.render(board.get_legal_moves(color)))
+
+
 def print_score(board):
     cell_distribution = board.compute_cell_distribution()
     print("\n#### SCORE (WHITE: {0}, BLACK: {1}) ####\n".format(cell_distribution[board.CELL_WHITE], cell_distribution[board.CELL_BLACK]))
+
+
+def get_reverse_color(color):
+    return Board.CELL_BLACK if color == Board.CELL_WHITE else Board.CELL_WHITE
