4c9a614805d4ee746694dff6fbf1e5319155a269
julien <julien@marmelab.com>
2016-10-25 15:34:03 +0200

    Code cleanup

--- src/game/game.py
+++ src/game/game.py
@@ -1,7 +1,6 @@
 from .board import Board
 
 def start():
-
     print("\n######### GAME STARTED ############\n")
 
     board = Board()
@@ -13,6 +12,5 @@ def start():
     #print(board.render(board.get_legal_moves(board.CELL_BLACK)))
 
 def print_score(board):
-
     cell_distribution = board.compute_cell_distribution()
     print("\n#### SCORE (WHITE: {0}, BLACK: {1}) ####\n".format(cell_distribution[board.CELL_WHITE], cell_distribution[board.CELL_BLACK]))
