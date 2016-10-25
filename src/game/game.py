35cb7e17681a33833e3e69c77a8c2014147661a1
julien <julien@marmelab.com>
2016-10-25 15:42:50 +0200

    Remove imbricated print

--- src/game/game.py
+++ src/game/game.py
@@ -6,7 +6,7 @@ def start():
     board = Board()
 
     print(board.render())
-    print(print_score(board))
+    print_score(board)
     print(board.render(board.get_legal_moves(board.CELL_WHITE)))
     print(board.get_flipped_disks_for_move(3, 2, board.CELL_WHITE))
     #print(board.render(board.get_legal_moves(board.CELL_BLACK)))
